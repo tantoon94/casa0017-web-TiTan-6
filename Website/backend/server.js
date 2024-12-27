4cffe4c4811ead917df51abdf9d9cfda3148d0c2
Dankao Chen <mianreiki@gmail.com>
2024-12-27 16:31:08 +0000

    Frontend fixed
    The .css and .js files was no effect to the web provided by the Nginx, now its fixed (path problem).

--- Website/backend/server.js
+++ Website/backend/server.js
@@ -110,6 +110,7 @@ app.get('/', async (req, res) => {
 
 app.listen(PORT, async () => {
   await connectToDatabase();
+  const localIP = getLocalIP();
   console.log(`Backend is running on http://localhost:${PORT}`);
   console.log(`Server is running on http://${localIP}:${PORT}`);
 });
